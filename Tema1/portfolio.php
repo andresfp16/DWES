0a41ac70232899dd33ac658738b985d9b32edf80
Andrés Rodriguez <a21romaan@iesgrancapitan.org>
2023-09-20 17:54:18 +0200

    portfolio

---
+++ Tema1/portfolio.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Meta y título aquí -->
+</head>
+<body>
+    <?php
+$nombre = "Andrés Rodríguez Machado";
+$correo = "a21romaan@iesgrancapitan.org";
+?>
+    <header>
+        <h1><?php echo $nombre; ?></h1>
+        <p>Contacto: <?php echo $correo; ?></p>
+    </header>
+</body>
+</html>
