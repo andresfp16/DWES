090c2bd6405b1367793c6dda0b430ee802fd963c
Andrés Rodriguez <a21romaan@iesgrancapitan.org>
2023-09-20 17:56:51 +0200

    portfolio

--- Tema1/portfolio.php
+++ Tema1/portfolio.php
@@ -14,5 +14,8 @@ $correo = "a21romaan@iesgrancapitan.org";
         <h1><?php echo $nombre; ?></h1>
         <p>Contacto: <?php echo $correo; ?></p>
     </header>
+    <footer>
+        <a href="https://github.com/andresfp16/DWES/blob/main/Tema1/portfolio.php">Enlace de github</a>
+    </footer>
 </body>
 </html>
